d265d97325d0e4c5c7bfab7d82339e6cb8effb4b
dsalasab <138070815+dsalasab@users.noreply.github.com>
2023-07-24 15:32:20 -0600

    Update Inspire.php
    To know its the Gihub one

--- src/Inspire.php
+++ src/Inspire.php
@@ -7,6 +7,6 @@ use Illuminate\Support\Facades\Http;
 class Inspire {
     public function justDoIt() {
         $response = Http::get('https://type.fit/api/quotes');
-        return $response[0]['text'] . ' -' . $response[0]['author'];
+        return $response[0]['author']." said that ".$response[0]['text'];
     }
 }
