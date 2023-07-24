54b1ba0de9acf36a59bc8d46bf582dddb399476a
dsalasab <138070815+dsalasab@users.noreply.github.com>
2023-07-24 15:47:09 -0600

    Update Inspire2.php
    To know its the Github one

--- src/Inspire2.php
+++ src/Inspire2.php
@@ -4,7 +4,7 @@ namespace Ghusr\Inspire2;
 
 use Illuminate\Support\Facades\Http;
 
-class Inspire {
+class Inspire2 {
     public function justDoIt() {
         $response = Http::get('https://type.fit/api/quotes');
         return $response[0]['author']." said that ".$response[0]['text'];
